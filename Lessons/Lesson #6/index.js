00153888e5a0bad25de972e12fea467b52a40295
Ron <139393192+rons-innovative-visions@users.noreply.github.com>
2023-12-31 14:09:31 -0500

    Add files via upload

---
+++ Lessons/Lesson #6/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'
+import {videos} from './data'
+import { useState } from 'react';
+
+const VideoList = () => {
+    return (
+        <div className='videolist'>
+            {videos.map((video) => {
+                return <Video {...video}></Video>
+            })}
+        </div>
+    )
+}
+
+const Video = ({thumbnail, logo, title, channelName, views}) => {
+
+    const [vidViews, setVidViews] = useState(views)
+
+    return (
+        <div className='video' onClick={() => setVidViews(vidViews+1)}>
+            <img className='thumbnail' src={thumbnail} alt="video thumbnail" />
+            <div className="video-content">
+                <img src={logo} alt="" />
+                <h2>{title}</h2>
+                <p>{channelName}</p>
+                <span>{vidViews} views</span>
+            </div>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<VideoList></VideoList>);
